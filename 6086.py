dbdc5cc9507965450de34092262a554309e2294d
HoyiTT <pjs9177@naver.com>
2021-06-25 07:15:18 +0000

    210625 6091

---
+++ 6086.py
@@ -0,0 +1,11 @@
+inp = int(input())
+
+num = 0
+sum = 0
+while True:
+    sum += 1
+    num +=sum
+    if inp <= num:
+        break
+print(num)
+    
---
+++ 6087.py
@@ -0,0 +1,5 @@
+num = int(input())
+for i in range(1, num+1):
+    if i % 3 == 0:
+        continue
+    print(i, end=' ')
