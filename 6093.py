cf2f5e843546cc837f8e2736120d1e241099f000
HoyiTT <pjs9177@naver.com>
2021-06-26 10:16:51 +0000

    210626

---
+++ 6093.py
@@ -0,0 +1,8 @@
+n = int(input())      #개수를 입력받아 n에 정수로 저장
+a = input().split()  #공백을 기준으로 잘라 a에 순서대로 저장
+
+for i in range(n) :  #0부터 n-1까지...
+  a[i] = int(a[i])       #a에 순서대로 저장되어있는 각 값을 정수로 변환해 다시 저장
+
+for  i in range(n-1, -1, -1):
+    print(a[i], end=' ')
