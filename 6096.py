3bbef818d92ecbeb6a4421b315b80d060db2ee59
HoyiTT <pjs9177@naver.com>
2021-06-27 12:22:31 +0000

    210627 v3

--- 6096.py
+++ 6096.py
@@ -1,6 +1,12 @@
+d=[]                        #대괄호 [ ] 를 이용해 아무것도 없는 빈 리스트 만들기
+for i in range(20) :
+  d.append([])         #리스트 안에 다른 리스트 추가해 넣기
+  for j in range(20) : 
+    d[i].append(0)    #리스트 안에 들어있는 리스트 안에 0 추가해 넣기
 n = int(input())
-x , y = input().split() 
-for i in range(n) :
+
+
+for i in range(n):
   x,y=input().split()
   for j in range(1, 20) :
     if d[j][int(y)]==0 :
@@ -12,3 +18,9 @@ for i in range(n) :
       d[int(x)][j]=1
     else :
       d[int(x)][j]=0
+
+
+for i in range(1, 20) :
+  for j in range(1, 20) : 
+    print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
+  print() 
