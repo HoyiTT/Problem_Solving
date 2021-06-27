49574f620aa6b465e7e64e83a6a0c1b3cb64b3f1
HoyiTT <pjs9177@naver.com>
2021-06-27 09:53:08 +0000

    210627 v2

--- 6095.py
+++ 6095.py
@@ -3,12 +3,20 @@ for i in range(20) :
   d.append([])         #리스트 안에 다른 리스트 추가해 넣기
   for j in range(20) : 
     d[i].append(0)    #리스트 안에 들어있는 리스트 안에 0 추가해 넣기
-    
 n = int(input())
-for i in range(n) :
-  x, y = input().split()
-  d[int(x)][int(y)] = 1
 
+for i in range(n):
+  x,y=input().split()
+  for j in range(1, 20) :
+    if d[j][int(y)]==0 :
+      d[j][int(y)]=1
+    else :
+      d[j][int(y)]=0
+
+    if d[int(x)][j]==0 :
+      d[int(x)][j]=1
+    else :
+      d[int(x)][j]=0
 for i in range(1, 20) :
   for j in range(1, 20) : 
     print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
