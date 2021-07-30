9268604e9439fd868742144e04798abf5515c90d
HoyiTT <pjs9177@naver.com>
2021-07-30 11:34:00 +0000

    BJ10828

---
+++ BAEKJOON/10828.py
@@ -0,0 +1,35 @@
+import sys
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N):
+    word = sys.stdin.readline().split()
+    order = word[0]
+    
+    if order == "push":
+        value = word[1]
+        stack.append(value)
+        
+    elif order == "pop":
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+            
+    elif order == "size":
+        print(len(stack))
+    
+    elif order == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+            
+    elif order == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+            
+    
+        
