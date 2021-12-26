eb6df5e4d910896d298a5b3c38d08c439aebedf4
박 준서 <pjs9177@naver.com>
2021-12-26 14:43:40 +0000

    ~BOJ 2739

---
+++ BAEKJOON/11721.py
@@ -0,0 +1,5 @@
+long  = input()
+a = len(long)
+for i in range(0,a,10):
+    b = i+10
+    print(long[i:b])
---
+++ BAEKJOON/11721_1.py
@@ -0,0 +1,5 @@
+long = input()
+n = len(long)
+for a in range(0,n,10):
+    b = a + 10
+    print(long[a:b])
