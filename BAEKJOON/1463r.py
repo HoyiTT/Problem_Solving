1ca7f2efd8abffb0b7b6f7e2533865df55c3cc5f
박 준서 <pjs9177@naver.com>
2022-05-07 12:03:10 +0000

    BOJ 1463,11726 Review

--- BAEKJOON/1463r.py
+++ BAEKJOON/1463r.py
@@ -2,6 +2,8 @@ n = int(input())
 
 dp = [0] * (n+1)
 
+print(dp)
+
 for i in range(2, n+1):
     dp[i] = dp[i-1] + 1
 
@@ -10,4 +12,5 @@ for i in range(2, n+1):
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i//3] + 1)
 
+    print(i,dp[i])
 print(dp[n])
