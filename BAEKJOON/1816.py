e119d541016b0da076045e4a58b97ed926cc5f0e
JUNSEO PARK <50609962+HoyiTT@users.noreply.github.com>
2022-10-02 04:10:00 +0000

    BOJ1816 DONE

--- BAEKJOON/1816.py
+++ BAEKJOON/1816.py
@@ -1,25 +1,14 @@
-def factorization(x):
-    d = 2
-    data = []
-    while d <= x:
-        if x % d == 0:
-            data.append(d)
-            x = x / d
-        else:
-            d = d + 1
-    for i in data:
-        if i > 10 ** 6:
-            continue
-        else:
-            print("NO")
-            return False
-    print("YES")
-    return True
-
-
 x = int(input())
-data2 = []
-for x in range(x):
-    data2.append(int(input()))
-for x in data2:
-    factorization(x)
+data = []
+
+for _ in range(x):
+    data.append(int(input()))
+
+for a in data:
+    for j in range(2, 1000001):
+        if a % j == 0:
+            ans = "NO"
+            break
+        elif j == 1000000:
+            ans = "YES"
+    print(ans)
