8c27e6d3ff9c8993d9bdaef4bbf958c75995f2eb
HoyiTT <pjs9177@naver.com>
2023-01-08 13:49:31 +0900

    BOJ 11722 Relearning

--- JAVA/BOJ/DP/11722.java
+++ JAVA/BOJ/DP/11722.java
@@ -5,33 +5,28 @@ class Main {
     Scanner scanner = new Scanner(System.in);
     int k = scanner.nextInt();
 
+    int [] seq = new int[k];
     int [] dp = new int[k];
-    int [] dt = new int[k];
 
     for(int a = 0; a < k; a++){
-      dt[a] = scanner.nextInt();
+      seq[a] = scanner.nextInt();
     }
 
     dp[0] = 1;
     int r = dp[0];
     
     for(int b = 1;b < k;++b){
-      //System.out.print ("b" + b + " ");
+      dp[b] = 1;
+      //dp모든 값의 최소값은 1이다.
       for(int c = 0; c < b;++c){
-        // System.out.print ("c" + c + " ");
-        if(dt[c] > dt[b]){
-          //System.out.print (dp[b]+ " " + dp[c] );
-          dp[b] = Math.max(dp[b],dp[c]);
+        if(seq[b] < seq[c] && dp[b] < dp[c] + 1){
+          dp[b] = dp[c] + 1;
         }
+        //b값이 c보다 작고 dp[b]가 dp[c] + 1보다 작을 경우 dp[b]에 dp[c] + 1을 대입
       }
-      dp[b] += 1;
-      r = Math.max(r,dp[b]);;
-      
+    r = Math.max(dp[b],r);
     }
-    
-		System.out.println(r);
-    
-    
+    System.out.println(r);
     scanner.close();
   }
 }
