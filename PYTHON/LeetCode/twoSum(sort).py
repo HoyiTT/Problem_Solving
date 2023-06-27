59cd1c876ef09aad047e27d7f5ca007eb7a3b23d
HoyiTT <pjs9177@naver.com>
2023-06-27 23:09:11 +0900

    [Feat] twoSum ing..

--- PYTHON/LeetCode/twoSum(sort).py
+++ PYTHON/LeetCode/twoSum(sort).py
@@ -4,10 +4,22 @@ https://leetcode.com/problems/two-sum/
 
 class Solution:
     def twoSum(self, nums, target):
+        orNums = nums
         nums.sort()
         l ,r = 0 , len(nums) - 1
         while l < r:
             if nums[l] + nums[r] > target: r -= 1
             elif nums[l] + nums[r] < target: l += 1
-            elif nums[l] + nums[r] == target: return nums[l] ,nums[r]
+            elif nums[l] + nums[r] == target: break
 
+        a , b = 0, 0
+
+        for i in len(nums):
+            if nums[l] == orNums[i]:
+                a = i
+        
+        for i in len(nums):
+            if nums[r] == orNums[i]:
+                b = i
+
+        return[a ,b]
