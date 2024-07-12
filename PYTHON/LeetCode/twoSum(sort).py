2d8c5e576f58fe513a394dd0a9a70f259f03a0e6
JUNSEO PARK <pjs9177@naver.com>
2024-07-12 16:03:30 +0900

    Update twoSum(sort).py

--- PYTHON/LeetCode/twoSum(sort).py
+++ PYTHON/LeetCode/twoSum(sort).py
@@ -1,25 +1,13 @@
-'''
-https://leetcode.com/problems/two-sum/
-'''
-
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        orNums = nums
-        nums.sort()
-        l ,r = 0 , len(nums) - 1
-        while l < r:
-            if nums[l] + nums[r] > target: r -= 1
-            elif nums[l] + nums[r] < target: l += 1
-            elif nums[l] + nums[r] == target: break
-
-        a , b = 0, 0
-
-        for i in len(nums):
-            if nums[l] == orNums[i]:
-                a = i
-        
-        for i in len(nums):
-            if nums[r] == orNums[i]:
-                b = i
-
-        return[a ,b]
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
